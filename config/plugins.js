f1e941a42362a34892eae905dbdc6e3e0e6b7564
Kenneth Coffie <markcoffiekenneth@gmail.com>
2024-03-31 12:28:51 +0100

    Add new plugins and dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -1,33 +1,36 @@
 module.exports = ({ env }) => ({
-  'users-permissions': {
+  "users-permissions": {
     config: {
-      jwtSecret: env('JWT_SECRET'),
+      jwtSecret: env("JWT_SECRET"),
     },
   },
 
   email: {
     config: {
-      provider: 'strapi-provider-email-resend',
+      provider: "strapi-provider-email-resend",
       providerOptions: {
-        apiKey: env('RESEND_API_KEY'), // Required
+        apiKey: env("RESEND_API_KEY"), // Required
       },
       settings: {
-        defaultFrom: 'hello@togetha.co.uk',
-        defaultReplyTo: 'hello@togetha.co.uk',
+        defaultFrom: "hello@togetha.co.uk",
+        defaultReplyTo: "hello@togetha.co.uk",
       },
-    }
+    },
   },
 
+  "import-export-entries": {
+    enabled: true,
+  },
+  
   upload: {
     config: {
-      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
       providerOptions: {
-        serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
+        serviceAccount: env.json("GCS_SERVICE_ACCOUNT"),
         bucketName: "strapi-togetha",
         publicFiles: true,
         uniform: false,
-        baseUrl: ""
-
+        baseUrl: "",
       },
 
       actionOptions: {
