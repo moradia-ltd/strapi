f29ed48f4de856374bc7b340e240598afc81e506
Kenneth Coffie <markcoffiekenneth@gmail.com>
2024-04-03 10:54:01 +0100

    Update middleware configuration and dependencies

--- config/plugins.js
+++ config/plugins.js
@@ -21,22 +21,44 @@ module.exports = ({ env }) => ({
   "import-export-entries": {
     enabled: true,
   },
-  
-  upload: {
+   upload: {
     config: {
-      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+      provider: "strapi-provider-cloudflare-r2",
       providerOptions: {
-        serviceAccount: env.json("GCS_SERVICE_ACCOUNT"),
-        bucketName: "strapi-togetha",
-        publicFiles: true,
-        uniform: false,
-        baseUrl: "",
+        accessKeyId: env("CF_ACCESS_KEY_ID"),
+        secretAccessKey: env("CF_ACCESS_SECRET"),
+    
+        endpoint: env("CF_ENDPOINT"),
+        params: {
+          Bucket: env("CF_BUCKET"),
+        },
+   
+        cloudflarePublicAccessUrl: env("CF_PUBLIC_ACCESS_URL"),
       },
-
       actionOptions: {
         upload: {},
+        uploadStream: {},
         delete: {},
       },
     },
   },
+ 
+
+  // upload: {
+  //   // config: {
+  //   //   provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
+  //   //   providerOptions: {
+  //   //     serviceAccount: env.json("GCS_SERVICE_ACCOUNT"),
+  //   //     bucketName: "strapi-togetha",
+  //   //     publicFiles: true,
+  //   //     uniform: false,
+  //   //     baseUrl: "",
+  //   //   },
+
+  //   //   actionOptions: {
+  //   //     upload: {},
+  //   //     delete: {},
+  //   //   },
+  //   // },
+  // },
 });
