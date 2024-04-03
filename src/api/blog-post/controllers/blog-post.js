ada95dc5efc48e19013109ceec9034cb128e9581
Kenneth Coffie <markcoffiekenneth@gmail.com>
2024-04-03 11:51:40 +0100

    Replace findOne method in blog-post controller

--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::blog-post.blog-post');
+module.exports = createCoreController('api::blog-post.blog-post', ({ strapi }) => ({
+
+    // Method 3: Replacing a core action
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const entity = await strapi.db.query('api::blog.blog').findOne({
+            where: { slug: id }
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
