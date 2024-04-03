1831dcd73228af21ee03d7ac6214f0c35673517f
Kenneth Coffie <markcoffiekenneth@gmail.com>
2024-04-03 12:36:18 +0100

    Fix the model name in findOne method

--- src/api/blog-post/controllers/blog-post.js
+++ src/api/blog-post/controllers/blog-post.js
@@ -11,7 +11,7 @@ module.exports = createCoreController('api::blog-post.blog-post', ({ strapi }) =
     // Method 3: Replacing a core action
     async findOne(ctx) {
         const { id } = ctx.params;
-        const entity = await strapi.db.query('api::blog.blog').findOne({
+        const entity = await strapi.db.query('api::blog-post.blog-post').findOne({
             where: { slug: id }
         });
         const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
